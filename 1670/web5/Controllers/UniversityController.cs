58a80a0f01bd14353d31123b9bf56f2857d24b88
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-09-29 11:37:35 +0700

    web5

---
+++ 1670/web5/Controllers/UniversityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web5.Models;
+
+namespace web5.Controllers
+{
+    public class UniversityController : Controller
+    {
+        //render ra view form để nhập liệu
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //xử lý dữ liệu nhập từ form
+        [HttpPost]
+        public IActionResult Index(University uni)
+        {    
+            return View("Result", uni);
+        }
+    }
+}
