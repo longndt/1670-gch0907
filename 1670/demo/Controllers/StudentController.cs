cd63af6e6c4c45438087f18745aaa7da33bc682f
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-10-08 09:57:52 +0700

    demo

--- 1670/demo/Controllers/StudentController.cs
+++ 1670/demo/Controllers/StudentController.cs
@@ -19,5 +19,25 @@ namespace demo.Controllers
         {
             return View(context.Students.ToList());
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                //nếu id không tìm thấy thì trả về not found
+                return NotFound();
+            }
+            else
+            {
+                //tìm ra object student có id được yêu cầu
+                var student = context.Students.Find(id);
+                //xóa object student vừa tìm thấy
+                context.Students.Remove(student);
+                //lưu lại thay đổi trong db
+                context.SaveChanges();
+                //quay trở lại trang index sau khi thành công
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
