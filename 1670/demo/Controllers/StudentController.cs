5ad5af64b57c28da728a3194b5bb3e17c6e981fd
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-10-08 09:32:31 +0700

    demo

---
+++ 1670/demo/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using demo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        //attribute
+        private readonly ApplicationDbContext context;
+
+        //constructor (auto-generate : Alt+Enter => Generate constructor)
+        public StudentController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(context.Students.ToList());
+        }
+    }
+}
